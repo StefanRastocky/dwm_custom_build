39660183d1febd409b9ac6290507ee51e8e8d06d
Stefan Rastocky <rastockystefan@gmail.com>
2023-09-02 14:35:54 +0200

    added hardware volume keys shortcut to script called testaudio in my scripts. This is copied from archlinux page on dunst. This sends a notification when volume changes as well as makes a sound

--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "monospace:size=12" };
+static const char dmenufont[]       = "monospace:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -46,6 +46,12 @@ static const Layout layouts[] = {
 
 /* key definitions */
 #define MODKEY Mod1Mask
+/* define the volume keys asofound by using xev */
+/*      key code             keysim */
+#define XF86AudioLowerVolume 0x1008ff11
+#define XF86AudioRaiseVolume 0x1008ff13
+#define XF86AudioMute 			 0x1008ff12
+
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -65,30 +71,33 @@ static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont,
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,												XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	/* modifier                     key            				function        argument */
+	{ MODKEY,                       XK_d,          				spawn,          {.v = dmenucmd } },
+	{ MODKEY,												XK_Return,     				spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_b,          				togglebar,      {0} },
+	{ MODKEY,                       XK_j,          				focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,          				focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_i,          				incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_d,          				incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_h,          				setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,          				setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_Return,     				zoom,           {0} },
+	{ MODKEY,                       XK_Tab,        				view,           {0} },
+	{ MODKEY|ShiftMask,             XK_q,          				killclient,     {0} },
+	{ MODKEY,                       XK_t,          				setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,          				setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,          				setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_space,      				setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,      				togglefloating, {0} },
+	{ MODKEY,                       XK_0,          				view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,          				tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,      				focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period,     				focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,      				tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period,     				tagmon,         {.i = +1 } },
+	{ 0, 														XF86AudioLowerVolume, spawn, 					SHCMD("testvolume 3db-")	},
+	{ 0,                            XF86AudioRaiseVolume, spawn, 					SHCMD("testvolume 3db+")	},
+	{ 0,                            XF86AudioMute, 				spawn, 					SHCMD("testvolume toggle")	},	
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
